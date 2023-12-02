cc83622a5bb07c9cdd3318c58aa32a59cde08888
pck-llight <23sunrin113@sunrint.hs.kr>
2023-12-02 17:10:29 +0900

    topNav 추가

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import {IoMdMenu} from "react-icons/io";
+
+const Header = styled.header`
+  width: 100%;
+  height: 80px;
+  position: relative;
+  top: 0;
+  background-color: white;
+  display: flex;
+  
+`;
+
+const NavTopMenuButton = styled.button`
+  width: 28px;
+  height: 28px;
+  cursor: pointer;
+  margin: 0;
+  padding: 0;
+  border: none;
+  background: none; 
+  margin-left: 40px;
+  margin-top: 26px;
+  background-color: #fff;
+`
+
+const MenuStatus = styled.div`
+  font-size: 20px;
+  font-family: suit-bold;
+  margin: 0;
+  padding: 0;
+  margin-top: 26px;
+  margin-left: 48px;
+  color: #BBC0C3;
+`
+const B = styled.b`
+  font-size: 22px;
+  color: blue;
+`
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+  function menu(){
+    console.log("버튼 눌림")
+  }
+  
+  return(
+    <>
+      <Header>
+        <NavTopMenuButton onClick={menu}><IoMdMenu size={28} /></NavTopMenuButton>
+        <MenuStatus>상태 : <B itemID={"menu"}> 달력</B></MenuStatus>
+      </Header>
+    </>
   );
 }
 
