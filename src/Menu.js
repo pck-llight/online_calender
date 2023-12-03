73ba21625013fc18d445fca5404aaf5bd1762efb
pck-llight <23sunrin113@sunrint.hs.kr>
2023-12-03 22:12:19 +0900

    사이드 메뉴 디자인 완성

--- src/Menu.js
+++ src/Menu.js
@@ -2,36 +2,88 @@ import Memo from "./components/Memo"
 import styled from "styled-components";
 import {HiCalendar} from "react-icons/hi";
 import React from "react";
+import {PiTagFill} from "react-icons/pi";
 
 //사이드 메뉴
 const SideMenu = styled.div`
   width: 400px;
-  height: 100%;
-  height: calc(100% - 80px);
+  height: calc(100vh - 80px);
+  overflow-y: hidden;
+  //height: calc(100% - 80px);
   background-color: #ffffff;
 `
-//Menu디자인
+//메모 컨테이너
 const MenuTitle =styled.div`
   margin: 0;
   padding: 0;
   font-size: 18px;
   color: #828587;
   padding-left: 40px;
-  
+`
+//메모 추가 버튼
+const MemoAddButton =styled.button`
+  padding: 5px 10px;
+  background-color: #3654F4;
+  border-radius: 8px;
+  border: none;
+  position: relative;
+  margin-left: 200px;
+  color: white;
+  font-weight: bold;
+`
+//메모 칸
+const MemoContainer = styled.div`
+  height: calc(100vh - 320px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  overflow-y: scroll;
+  &::-webkit-scrollbar-thumb{
+    background-color: #00000032;
+    border-radius: 1.5px;
+  }
+  &::-webkit-scrollbar{
+    background-color: none;
+    border-radius: 999px;
+    width: 5px;
+  }
 `
 //메뉴 달력 버튼
 const MenuCalenderButton =styled.div`
   padding: 10px 0 10px 15px;
   align-items: center;
   width: 273px;
-  // height: 44px;
   border-radius: 999px;
   background-color: #EBEEFE;
   color: 3654F4;
+  text-align: left;
+  display: flex;
   align-items: center;
-  horiz-align: right;
+  justify-content: left;
+  color: black;
+  text-indent: 18px;
+  margin-top: 32px;
+  font-weight: bold;
 `
-
+//메뉴 메모 버튼
+  const MenuMemoButton =styled.div`
+    padding: 10px 0 10px 15px;
+    align-items: center;
+    width: 273px;
+    border-radius: 999px;
+    background-color: #EBEEFE;
+    color: black;
+    text-align: left;
+    display: flex;
+    align-items: center;
+    justify-content: left;
+    color: 3654F4;
+    text-indent: 18px;
+    margin-top: 16px;
+    font-weight: bold;
+    margin-bottom: 20px;
+`
 //hr 디자인
 const Hr = styled.hr`
   width: 330px;
@@ -48,13 +100,18 @@ function Menu(){
       <SideMenu>
         <MenuTitle>
           메뉴
-            <MenuCalenderButton><HiCalendar size={24} color={"#3654F4"}/></MenuCalenderButton>
+          <MenuCalenderButton><HiCalendar size={24} color={"#3654F4"}/>달력</MenuCalenderButton>
+          <MenuMemoButton><PiTagFill size={24} color={"#F44336"}/>메모 열람</MenuMemoButton>
+          메뉴 목록 <MemoAddButton>추가</MemoAddButton>
         </MenuTitle>
         <Hr/>
-        <Memo/>
-        <Memo/>
-        <Memo/>
-        <Memo/>
+        <MemoContainer>
+          <Memo/>
+          <Memo/>
+          <Memo/>
+          <Memo/>
+          <Memo/>
+        </MemoContainer>
       </SideMenu>
     </>
   )
--- src/components/Memo.js
+++ src/components/Memo.js
@@ -9,43 +9,37 @@ const MemoCard = styled.div`
   border-radius: 16px;
   background-color: #F3F5F6;
   border: none;
-  padding: 0;
+  padding: 10px;
   margin: 0;
-  margin-left: 40px;
-  margin-bottom: 24px;
-  display: inline-flex;
-  //align-items: ;
+  //margin-left: 40px;
+  //margin-bottom: 24px;
 `
 //메모 삭제 버튼
 const MemoDelButton = styled.button`
-  position: absolute;
+  position: relative;
   background-color: #ccd0da;
   border: none;
   width: 30px;
   height: 30px;
-  border-radius: 8px
+  border-radius: 8px;
+  transform: translateX(300px) translateY(-28px);
+  display: flex;
+  align-items: center;
 `
 //메모 제목
 const MemoTitle = styled.h3`
   color: #3654F4;
   margin: 0;
-  margin-left: 20px;
-  margin-bottom: 20px;
 `
 //메모 내용
 const MemoContent = styled.h4`
   margin: 0;
-  margin-left: 20px;
-  margin-bottom: 18px;
 `
 //메모 날짜
 const Memoday = styled.p`
   color: #BBC0C3;
   font-size: 14px;
   margin: 0;
-  margin-left: 20px;
-  margin-bottom: 18px;
-  
 `
 
 
@@ -58,12 +52,7 @@ function Memo(){
           <MemoTitle>제목</MemoTitle><br/>
           <MemoContent>내용</MemoContent><br/>
           <Memoday>2023.12.12</Memoday><br/>
-          <MemoDelButton style={{
-            transform: "translateX(290px) translateY(95px)",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}><MdDelete size={24}/></MemoDelButton>
+          <MemoDelButton><MdDelete size={24}/></MemoDelButton>
         </MemoCard>
     </>
   )
