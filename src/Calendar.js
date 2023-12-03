69c8a864fa937cf787ac6ceaab84794a6ca00b70
pck-llight <23sunrin113@sunrint.hs.kr>
2023-12-04 08:30:56 +0900

    달력 만들기 2트

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,8 +1,10 @@
+
+//달력
 import styled from "styled-components";
 import {IoChevronBackSharp, IoChevronForwardSharp} from "react-icons/io5";
 import {useState} from "react";
 
-//달력
+
 const CalendarContanier = styled.div`
   background-color: #FFF;
   margin: 40px;
@@ -16,115 +18,67 @@ const SelectDate= styled.div`
   //width: 90px;
   height: 29px;
   text-align: center;
-  font-weight: bold;
+  //font-weight: bold;
   font-size: 24px;
 `
-// 달력 - 요일
+// // 달력 - 요일
 const DayOfWeek = styled.div`
   color: #3E4143;
   font-size: 24px;
 `
-//달력 날짜
-const DateCell = styled.div`
-  //width: 30px;
-  //height: 30px;
-  //line-height: 30px;
-  margin: 2px;
-  // background-color: ${props => (props.empty ? '#ecf0f1' : 'white')};
-  //border: 1px solid #ccc;
-  text-align: center;
-`;
 
-const DayBox = styled.
+function Calendar(props){
 
-//달력 생성 함수
-function generateCalendar(year, month) {
-  const daysInMonth = new Date(year, month + 1, 0).getDate();
-  const firstDayOfMonth = new Date(year, month, 1).getDay();
-
-  const calendar = [];
-  let dayCounter = 1;
-
-  for (let i = 0; i < 6; i++) {
-    const week = [];
-
-    for (let j = 0; j < 7; j++) {
-      if ((i === 0 && j < firstDayOfMonth) || dayCounter > daysInMonth) {
-        week.push(null);
-      } else {
-        week.push(dayCounter);
-        dayCounter++;
-      }
-    }
-
-    calendar.push(week);
-  }
-
-  return calendar;
-}
-
-  //달력 출력 함수
-function Calendar(props) {
   const [selectedMonth, setSelectedMonth] = useState(new Date());
 
-  const renderCalendar = () => {
+  //달력 생성 함수
+
+
+  function GenerateCalendar() {
     const year = selectedMonth.getFullYear();
     const month = selectedMonth.getMonth();
-    const calendar = generateCalendar(year, month);
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+    const firstDayOfMonth = new Date(year, month, 1).getDay();
 
-    return calendar.map((week, weekIndex) => (
-      <div key={weekIndex} className="week">
-        {week.map((day, dayIndex) => (
-          <DateCell key={dayIndex} empty={day === null}>
-            {day}
-          </DateCell>
-        ))}
-      </div>
-    ));
-  };
+    const calendar = [];
+    let dayCounter = 1;
 
+    const DateCell = styled.div`
+      width: 50px;
+      height: 50px;
+      font-size: 24px;
+    `
+
+    for (let i = 0; i < 6; i++) {
+      const week = [];
+
+      for (let j = 0; j < 7; j++) {
+        if ((i === 0 && j < firstDayOfMonth) || dayCounter > daysInMonth) {
+          week.push(null);
+        } else {
+          week.push(dayCounter);
+          dayCounter++;
+        }
+      }
+
+      calendar.push(week);
+    }
+
+
+    return(
+      calendar.forEach(<DateCell></DateCell>)
+    )
+  }
 
 
   return(
     <CalendarContanier style={{
-      width: props.menu ? "calc(100vw - 410px)" : "100vw",
-      transition: "all .5s",
+      width: props.menu ? "calc(100vw - 410px)" : "100vh"
     }}>
-      <div style={{
-      }}>
-        <div style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          gap: "48px",
-          marginBottom: "20px"
-        }}>
-          <IoChevronBackSharp  size={24} color={"#828587"}/>
-          <SelectDate>11월 달력</SelectDate>
-          <IoChevronForwardSharp size={24} color={"#828587"}/>
-        </div>
-        <DayOfWeek>일</DayOfWeek>
-        <DayOfWeek>월</DayOfWeek>
-        <DayOfWeek>화</DayOfWeek>
-        <DayOfWeek>수</DayOfWeek>
-        <DayOfWeek>목</DayOfWeek>
-        <DayOfWeek>금</DayOfWeek>
-        <DayOfWeek>토</DayOfWeek>
-        <div style={{
-          display: "flex",
-          // flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "space-between",
-          // margin:"0px",
-          // padding:"0px"
-        }}>
-          {renderCalendar()}
-          {/*<tr>{renderCalendar()}</tr>*/}
-        </div>
+      <SelectDate><IoChevronBackSharp size={24} color={"#828587"}/>{selectedMonth.getMonth() + 1}월 달력<IoChevronForwardSharp size={24} color={"#828587"}/></SelectDate>
 
-      </div>
     </CalendarContanier>
-  );
+  )
 }
 
 export default Calendar
