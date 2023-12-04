7b6ffad0c2be72a94149628eda47b6940752a998
pck-llight <23sunrin113@sunrint.hs.kr>
2023-12-04 10:05:43 +0900

    기본 달력 완료 현재 달을 자동으로 인식하여 맞춤

--- src/Calendar.js
+++ src/Calendar.js
@@ -10,6 +10,7 @@ const CalendarContanier = styled.div`
   margin: 40px;
   border-radius: 16px;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 `
@@ -27,10 +28,16 @@ const DayOfWeek = styled.div`
   font-size: 24px;
 `
 
+
+const DateCell = styled.div`
+      width: 50px;
+      height: 50px;
+      font-size: 24px;
+    `
+
 function Calendar(props){
 
   const [selectedMonth, setSelectedMonth] = useState(new Date());
-
   //달력 생성 함수
 
 
@@ -39,44 +46,71 @@ function Calendar(props){
     const month = selectedMonth.getMonth();
     const daysInMonth = new Date(year, month + 1, 0).getDate();
     const firstDayOfMonth = new Date(year, month, 1).getDay();
+    const today = new Date();
+
 
     const calendar = [];
     let dayCounter = 1;
 
-    const DateCell = styled.div`
-      width: 50px;
-      height: 50px;
-      font-size: 24px;
-    `
 
     for (let i = 0; i < 6; i++) {
       const week = [];
 
+      // 7일(열)까지 반복
       for (let j = 0; j < 7; j++) {
+        // 첫 주이면서 현재 날짜 이전이거나 해당 월의 마지막 날을 넘어갔으면 null 추가
         if ((i === 0 && j < firstDayOfMonth) || dayCounter > daysInMonth) {
           week.push(null);
         } else {
+          // 아니면 날짜 추가
           week.push(dayCounter);
           dayCounter++;
         }
       }
 
+      // 주(행) 추가
       calendar.push(week);
     }
 
-
-    return(
-      calendar.forEach(<DateCell></DateCell>)
+    return (
+      <>
+        {
+          calendar.map((e) => {
+            return <div style={{
+              display: "flex",
+              textAlign: "center",
+            }}>
+              {
+                e.map((e) => {
+                    return <DateCell>{e}</DateCell>
+                  }
+                )
+              }
+            </div>
+            }
+          )
+        }
+      </>
     )
+
   }
 
 
   return(
     <CalendarContanier style={{
-      width: props.menu ? "calc(100vw - 410px)" : "100vh"
+      width: props.menu ? "calc(100vw - 410px)" : "100vh",
+      display:"flex",
+      alignItems:"center",
+      justifyContent:"space-between",
+      textAlign:"center"
     }}>
-      <SelectDate><IoChevronBackSharp size={24} color={"#828587"}/>{selectedMonth.getMonth() + 1}월 달력<IoChevronForwardSharp size={24} color={"#828587"}/></SelectDate>
-
+      <div style={{
+        display:"flex",
+        gap: "20px"
+      }}>
+        <SelectDate><IoChevronBackSharp size={24} color={"#828587"}/>{selectedMonth.getMonth() + 1}월 달력<IoChevronForwardSharp size={24} color={"#828587"}/></SelectDate>
+      </div>
+      <GenerateCalendar/>
     </CalendarContanier>
   )
 }
